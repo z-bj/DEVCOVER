7dc59d14185ed674ae7da797e65e4db50aaf625e
Bj'z <zbjarchi@gmail.com>
2023-03-14 08:51:56 +0200

    :lipstick: Header.module.css

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>DEVCOVER</h1>
+      
+    </header>
+  );
+};
+
+export default Header;
