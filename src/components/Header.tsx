203cb9088bccac6cf28f8043d16d261460b6324e
Bj'z <zbjarchi@gmail.com>
2023-03-14 09:00:37 +0200

    :lipstick: update UI App.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./Header.module.css";
-
+import Toggle from "./Toggle";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1>DEVCOVER</h1>
-      
+      <Toggle/>
     </header>
   );
 };
