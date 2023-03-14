fbb90905fad659d0a289e57cb8ab07234892acb2
Bj'z <zbjarchi@gmail.com>
2023-03-14 07:58:26 +0200

    :lipstick: update: CSS
    App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-    </div>
+<div></div>
   )
 }
 
