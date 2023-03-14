26489718caf5be6c28d8da4f258a9f5a19c50a31
Bj'z <zbjarchi@gmail.com>
2023-03-14 11:15:33 +0200

    :bug: Add Main inner the <><>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css"
 import React, {Fragment} from "react"
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 
 function App() {
   return (
-<Fragment><Header /></Fragment>
+    <Fragment>
+      <Header />
+      <Main />
+    </Fragment>
   )
 }
 
