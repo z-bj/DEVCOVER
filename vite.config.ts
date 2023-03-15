ef068862785dd16c528e653038e9d8e3bcc9073d
Bj'z <zbjarchi@gmail.com>
2023-03-15 12:58:57 +0200

    :green_heart: fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "DEVCOVER",
-  plugins: [react(),svgr({ 
-      svgrOptions: {
-        // svgr options
-      },
-    })],
+  plugins: [react()],
 })
